7f84dcb43923155c504030a4b5d0822c39e4a057
pcheh <konstantein.lebedev@mail.ru>
2021-09-21 00:44:00 +0500

    Task completed

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Ода о мухе и варенье");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Села муха на варенье - вот и все стихотворение");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
