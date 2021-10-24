ec2365600fcaa3fdee0ab6af68ae34ba804a4a37
pcheh <konstantein.lebedev@mail.ru>
2021-10-24 18:19:25 +0500

    task 3 complete

---
+++ Task 3/Task 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            var x = double.Parse(Console.ReadLine());
+            double y = Math.Sqrt((2 * x + Math.Sin(Math.Abs(3 * x))) / 3.56);
+            Console.WriteLine(y);
+
+            Console.ReadKey();
+        }
+    }
+}
