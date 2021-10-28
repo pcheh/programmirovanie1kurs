833d6d5e1b6e918cda655fd6c72fc8cd8773057d
pcheh <konstantein.lebedev@mail.ru>
2021-10-28 17:53:14 +0500

    task 4 done

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = SquareSum(2, SquareSum(3, SquareSum(4, SquareSum(5, 0))));
+            Console.WriteLine($"x = {Math.Round(x, 3)}");
+
+            Console.ReadKey();
+        }
+
+        static double SquareSum(double a, double b)
+        {
+            return Math.Sqrt(a + b);
+        }
+    }
+}
