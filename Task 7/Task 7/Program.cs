2910540ff5288845ca7813c15745340079af733c
pcheh <konstantein.lebedev@mail.ru>
2021-12-26 17:02:28 +0500

    done

---
+++ Task 7/Task 7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //вариант 3
+            Console.WriteLine("Введите x");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("f(x) = " + CountFunction(x));
+
+            Console.ReadKey();
+        }
+
+        static double CountFunction (double x)
+        {
+            if (x > 2)
+                return 2;
+            else
+                if (x > 0 && x <= 2)
+                    return 0;
+                return -3 * x;
+        }
+    }
+}
